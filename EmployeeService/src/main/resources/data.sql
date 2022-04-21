bf3fde6cf097dc7e5788cff7a9e8f85f2004b0e3
Jinumol K J <jinumolkj@gmail.com>
2022-04-22 00:59:10 +0530

    First commit

---
+++ EmployeeService/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into employee values(1,'jinu@gmail.com','Jinu','mol');
+insert into roles values(1,'ADMIN');
+insert into roles values(2,'USER');
+insert into users values(1,'$2a$12$XaPjKvMs4hf.2F4FNvfr5ueild2HM9DHH6FxYqyynfjTPZM2r005e','Jinu');
+insert into users values(2,'$2a$12$1ZUaWzkodsu8KKTLLvUUMODNH8rbD8QPbLog2Np7/0dRyE8a/UP9O','Juan');
+insert into users_roles values(1,1);
+insert into users_roles values(2,2);
